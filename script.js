5da0cd82e468d0acac9cc626c437a905799a0704
Julie <117595656+juliechan01@users.noreply.github.com>
2023-02-15 16:51:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function over(element) {
+    element.style.backgroundColor = "skyblue";
+}
+
+function out(element) {
+    element.style.backgroundColor = "royalblue";
+}
+
+function emailButton() {
+    document.getElementById("email").style.backgroundColor = "rgb(246,236,190)";
+}
+
+function emailOut() {
+    document.getElementById("email").style.backgroundColor = "goldenrod";
+}
